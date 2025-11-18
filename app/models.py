282144d161fd06b8b2cd1c7fd38c2ec5da081087
mlipoecom <martin.liporace@ecom.uy>
2025-11-18 15:21:05 -0300

    Subo models

--- app/models.py
+++ app/models.py
@@ -1,21 +1,14 @@
 from pydantic import BaseModel, Field, EmailStr
-from typing import Optional
+from typing import Optional, List
 
 class UserGenerate(BaseModel):
     name: str = Field(..., max_length=50, example="John", description="Nombre del usuario")
     lastName: str = Field(..., max_length=50, example="Doe", description="Apellido del usuario")
-    email: EmailStr = Field(..., max_length=50, example="john.doe@ecom.com.uy", description="Email del usuario")
+    email: EmailStr = Field(..., example="john.doe@ecom.com.uy", description="Email del usuario")
     externalId: str = Field(..., max_length=50, example="johnDoe123", description="Identificador externo del usuario")
+    supplierId: Optional[int] = Field(None, example=1, description="ID del proveedor al que pertenece el usuario")
     companyId: Optional[int] = Field(None, example=1, description="ID de la compañía a la que pertenece el usuario")
     customerId: Optional[int] = Field(None, example=1, description="ID del cliente al que pertenece el usuario")
-    status: str = Field('activo', description="Estado inicial del usuario ('activo', 'suspendido', 'inactivo')")
-
-class UserGenerate(BaseModel):
-    name: str = Field(..., max_length=50, example="John", description="Nombre del usuario")
-    lastName: str = Field(..., max_length=50, example="Doe", description="Apellido del usuario")
-    email: EmailStr = Field(..., max_length=50, example="john.doe@ecom.com.uy", description="Email del usuario")
-    externalId: str = Field(..., max_length=50, example="johnDoe123", description="Identificador externo del usuario")
-    companyId: int = Field(..., example=1, description="ID de la compañía a la que pertenece el usuario")
     status: str = Field('activo', description="Estado inicial del usuario ('activo', 'suspendido', 'inactivo')")
 
 class CompanyGenerate(BaseModel):
@@ -25,7 +18,7 @@ class CompanyGenerate(BaseModel):
     description: str = Field(..., max_length=256, example="Consultance services", description="Descripción de la empresa")
     status: str = Field('activo', description="Estado inicial de la empresa ('activo', 'suspendido', 'inactivo')")
     supplierId: int = Field(..., example=1, description="ID del proveedor")
-    email: EmailStr = Field(..., max_length=50, example="info@acme.com", description="Email de la empresa")
+    email: EmailStr = Field(..., example="info@acme.com", description="Email de la empresa")
 
 class CompanyGenerateResponse(BaseModel):
     info: str = Field(example="Compañía creada exitosamente.")
@@ -37,7 +30,7 @@ class SupplierGenerate(BaseModel):
     externalId: str = Field(..., max_length=50, example="987654321223", description="Identificador externo del proveedor")
     description: str = Field(..., max_length=256, example="Software Factory", description="Descripción del proveedor")
     status: str = Field('activo', description="Estado inicial del proveedor ('activo', 'suspendido', 'inactivo')")
-    email: EmailStr = Field(..., max_length=50, example="info@ecom.com.uy", description="Email del proveedor")
+    email: EmailStr = Field(..., example="info@ecom.com.uy", description="Email del proveedor")
 
 class SupplierGenerateResponse(BaseModel):
     info: str
@@ -66,3 +59,8 @@ class MessageResponse(BaseModel):
 class AssignRolesRequest(BaseModel):
     user_id: int = Field(..., example=1, description="ID del usuario")
     role_id: int = Field(..., example=1, description="ID del rol")
+
+class AssignClientRequest(BaseModel):
+    userId: int
+    customerId: int
+    categorieId: List[int]
